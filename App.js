7a18ccae4873d57518271e251cfac706c99189ba
Francesco Wild <francescowild90@gmail.com>
2018-08-19 21:17:24 +0100

    upd

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Image source={require('./assets/icon.png')}/>
+        <TouchableOpacity 
+          //onPress=
+          style={styles.button}
+        >
+          <Text
+            style={styles.buttonText}
+          >TrovAli</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -14,8 +22,18 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#dd9933',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button:{
+    alignItems: 'center',
+    backgroundColor: '#215c84',
+    padding: 10,
+    borderRadius: 5
+  },
+  buttonText :{
+    color: '#fff',
+    fontSize: 18,
+  }
 });
